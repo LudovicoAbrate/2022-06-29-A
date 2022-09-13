1dd8c8c026dd3bcad75325ccc374ec8362fd778c
ludovicoabrate <ludovicoabrate@localhost>
2022-09-13 23:23:13 +0200

    punto 1bis

--- src/main/java/it/polito/tdp/itunes/Main.java
+++ src/main/java/it/polito/tdp/itunes/Main.java
@@ -3,5 +3,7 @@ package it.polito.tdp.itunes;
 public class Main {
 	public static void main(String[] args) {
         EntryPoint.main(args);
-    }
+    //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
+	
+	}
 }
